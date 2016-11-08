c581944c3e1e8af900d88718189b8137dc663409
Juan Manuel Caicedo <jmcaicedo1@gmail.com>
2016-11-07 21:40:49 -0500

    Eureka Discovery Server created

---
+++ micro-discovery-server/src/main/java/com/devstepsblog/DiscoveryServerConfig.java
@@ -0,0 +1,13 @@
+package com.devstepsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerConfig {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DiscoveryServerConfig.class, args);
+	}
+	
+}
